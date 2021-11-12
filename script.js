5412dd3304f43b3163310abd015ec1d7a1969ae9
Mukund Mundhra <mukund.mundhra@moveinsync.com>
2021-11-12 15:50:03 +0530

    get request template added

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Dart Project!')
+})
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/newProduct', (req, res) => {
+  res.send('new!');
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
